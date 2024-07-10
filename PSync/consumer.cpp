38a6d8691fc51d9970e2c5d8537affe2ca4a0538
suraviregmi <regmisuravi@gmail.com>
2024-07-10 15:57:17 -0400

    add latest seq number to .ndn/psync directory

--- PSync/consumer.cpp
+++ PSync/consumer.cpp
@@ -29,6 +29,7 @@
 #include <pwd.h>
 #include <cstdlib>
 #include <unistd.h>
+#include <sys/stat.h>
 
 namespace psync {
 
@@ -67,29 +68,46 @@ Consumer::Consumer(const ndn::Name& syncPrefix,
 }
 
 
+
+
 void
 Consumer::writeSeqNoToFile( const std::optional <uint64_t> seq)
 {
-  m_seqFileNameWithPath = "";
 
-  if (m_seqFileNameWithPath.empty()) {
-    std::string homeDirPath(getpwuid(getuid())->pw_dir);
-    if (homeDirPath.empty()) {
-      homeDirPath = getenv("HOME");
-    }
-    m_seqFileNameWithPath = homeDirPath;
+  // find home directory  
+  const char* homeDir = getenv("HOME");
+  if (homeDir == nullptr) {
+      NDN_LOG_DEBUG("Home directory variable not set");
+      return;
   }
-  m_seqFileNameWithPath = m_seqFileNameWithPath + "/pSyncSeqNo.txt";
-  NDN_LOG_DEBUG("path is " << m_seqFileNameWithPath);
+  // get path to psync directory
+  std::string path = std::string(homeDir) + "/.ndn/psync/";
+  struct stat info;
+  if (stat(path.c_str(), &info) != 0) {
+    //create directory if not exist
+    mkdir(path.c_str(), 0755);
+  }
+  // replace / with -
+  std::string file_name = m_syncPrefix.toUri();
+  for (char& ch : file_name) {
+    if (ch == '/') {
+        ch = '-';
+    }
+  }
+  if (!file_name.empty() && file_name[0] == '-') {
+        file_name.erase(0, 1);
+  }
+
+  m_seqFileNameWithPath = path + file_name +".txt";
 
   std::ofstream outputFile(m_seqFileNameWithPath.c_str());
   std::ostringstream os;
   os << "Seq No " <<  (seq.has_value() ? std::to_string(seq.value()) : "N/A") << "\n";
   outputFile << os.str();
+  NDN_LOG_DEBUG("inserted seq no to file" << m_seqFileNameWithPath);
 
   outputFile.close();
 }
-
 
 
 bool
